011a2e9a0702e3fcb6558a11759df26ddcd3e97a
mishamyrt <misha@myrt.co>
2021-05-17 16:54:55 +0300

    index: Clean up

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import 'module-alias/register';
-// import { getDayPostcards } from '@modules/postcards';
 import { randomItem } from '@modules/arrays';
 import { postMessage } from '@modules/discord';
 import { getDayPostcards } from '@modules/postcards';
 
 const url = process.env.WEBHOOK_URL
+
+const envError = 'Please specify a WEBHOOK_URL environment variable'
 
 function handleError (err: Error): never {
   console.error(err)
@@ -12,9 +13,7 @@ function handleError (err: Error): never {
 }
 
 if (!url) {
-  handleError(
-    new Error('Please specify a WEBHOOK_URL environment variable')
-  )
+  handleError(new Error(envError))
 }
 
 const day = new Date().getDay()
